09a31c6b0a9204fb4cec20abb53fcf5ad4ec50d6
Connor G Meehan <connorgm@pm.me>
2024-01-05 19:37:51 +1100

    refact(bb_vector_network): Deleted old version, new version is default

--- packages/bb_vector_network/src/lib.rs
+++ packages/bb_vector_network/src/lib.rs
@@ -1,19 +1,10 @@
-// mod bbanchor;
-// mod bbvectornetwork;
-// mod bbvnlink;
-// mod bbindex;
-// mod bbvnregion;
-mod traits;
-#[cfg(feature = "debug_draw")]
-mod debug_draw;
+pub mod bb_node;
+pub mod bb_edge;
+pub mod bb_region;
+pub mod bb_graph;
+pub mod errors;
+pub mod traits;
 
-pub mod impl2;
+pub mod bb_graph_lyon_path;
 
-// pub use bbvectornetwork::BBVectorNetwork;
-// pub use bbvnlink::BBVNLink;
-// pub use bbvnregion::{BBVNRegion, BBVNWindingRule};
-pub use impl2::bb_graph::BBGraph;
-pub use impl2::bb_edge::{BBEdge, BBEdgeIndex};
-pub use impl2::bb_node::{BBNode, BBNodeIndex};
-pub use traits::Determinate;
-pub use impl2::prelude;
+pub mod prelude;
--- packages/bb_vector_network/src/traits.rs
+++ packages/bb_vector_network/src/traits.rs
@@ -9,34 +9,3 @@ impl Determinate<f32> for Vec2 {
         self.x * other.y - self.y * other.x
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use glam::Vec2;
-
-    use super::Determinate;
-
-    #[test]
-    fn test_determinate() {
-        let v_up = Vec2::new(0., 1.);
-        let v_left = Vec2::new(-1., 0.);
-        let v_right = Vec2::new(1., 0.);
-        let v_down = Vec2::new(0., -1.);
-
-        let up_left_det = v_up.determinate(v_left);
-        assert_eq!(up_left_det, 1., "up_left_det");
-        let up_right_det = v_up.determinate(v_right);
-        assert_eq!(up_right_det, -1., "up_right_det");
-        let up_down_det = v_up.determinate(v_down);
-        assert_eq!(up_down_det, -0., "up_down_det");
-
-        let left_down_det = v_left.determinate(v_down);
-        assert_eq!(left_down_det, 1., "left_down_det");
-
-        let left_right_det = v_left.determinate(v_right);
-        assert_eq!(left_right_det, -0., "left_right_det");
-
-        let down_right_det = v_down.determinate(v_right);
-        assert_eq!(down_right_det, 1., "down_right_det");
-    }
-}
