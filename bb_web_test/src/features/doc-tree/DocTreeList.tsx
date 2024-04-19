e4ab7e95d5623bc89647f8cf736deeb6242c4179
Connor Meehan <connorgm@pm.me>
2024-04-19 15:50:42 +1000

    feat,refact: Seperates DocTreeNode, improves styles for doctree button

--- bb_web_test/src/features/doc-tree/DocTreeList.tsx
+++ bb_web_test/src/features/doc-tree/DocTreeList.tsx
@@ -1,117 +1,7 @@
-import { DetailedObject } from 'bb_core';
-import { For, Show, createEffect, createMemo, createSignal } from 'solid-js';
-import {
-    TbChevronDown,
-    TbChevronRight,
-    TbEye,
-    TbEyeClosed,
-    TbFocus,
-} from 'solid-icons/tb';
+import { For, createEffect, createMemo } from 'solid-js';
 import { useBobbinBear } from '../../hooks/useBobbinBear';
-import { cn } from '../../lib/utils';
-import { isDefined } from '~/utils/typeguards';
+import { DocTreeNode } from './DocTreeNode';
 
-import { Button } from '../../components/button';
-import {
-    Collapsible,
-    CollapsibleContent,
-    CollapsibleTrigger,
-} from '~/components/ui/collapsible';
-
-type DocTreeNodeProps = {
-    object: DetailedObject;
-    indent: number;
-};
-export function DocTreeNode(props: DocTreeNodeProps) {
-    const { document } = useBobbinBear();
-    const { setVisible, selectSingle } = document;
-    const [expanded, setExpanded] = createSignal(true);
-
-    const childObjects = createMemo(() => {
-        if (!props.object.children) return;
-        const childObjects = props.object.children
-            .map((uid) => document.objects.get(uid))
-            .filter(isDefined);
-        if (childObjects.length === 0) return undefined;
-        else return childObjects;
-    });
-
-    createEffect(() => {
-        console.log(
-            `object(${props.object.uid}) child objects`,
-            childObjects(),
-        );
-    });
-    return (
-        <Collapsible
-            style={{ 'margin-left': `${props.indent * 12}px` }}
-            open={expanded()}
-            onOpenChange={(open) => setExpanded(open)}
-        >
-            <div
-                class={cn(
-                    'flex items-center gap-2 select-none hover:outline hover:outline-1 outline-yellow-600 py-1',
-                    props.object.selected && 'bg-orange-100',
-                )}
-                onClick={async (e) => {
-                    selectSingle(props.object.uid);
-                    e.stopPropagation();
-                }}
-            >
-                <Show when={childObjects()}>
-                    <CollapsibleTrigger onClick={(e) => e.stopPropagation()}>
-                        <Button
-                            size="tiny"
-                            class="bg-transparent bg-opacity-50 hover:bg-orange-50"
-                        >
-                            <Show
-                                when={expanded()}
-                                fallback={<TbChevronRight />}
-                            >
-                                <TbChevronDown />
-                            </Show>
-                        </Button>
-                    </CollapsibleTrigger>
-                </Show>
-
-                <Button
-                    size="tiny"
-                    class="bg-transparent bg-opacity-50 hover:bg-orange-50"
-                    onClick={(e) => {
-                        setVisible(props.object.uid, !props.object.visible);
-                        e.stopPropagation();
-                    }}
-                >
-                    <Show
-                        when={props.object.visible}
-                        fallback={<TbEyeClosed />}
-                    >
-                        <TbEye />
-                    </Show>
-                </Button>
-                <Show when={props.object.inspected}>
-                    <TbFocus />
-                </Show>
-
-                {props.object.name ?? 'Unknown'}
-            </div>
-            <Show when={childObjects()}>
-                {(children) => (
-                    <CollapsibleContent>
-                        <For each={children()}>
-                            {(object) => (
-                                <DocTreeNode
-                                    object={object}
-                                    indent={props.indent + 1}
-                                />
-                            )}
-                        </For>
-                    </CollapsibleContent>
-                )}
-            </Show>
-        </Collapsible>
-    );
-}
 
 export function DocTreeList() {
     const { document } = useBobbinBear();
@@ -128,7 +18,7 @@ export function DocTreeList() {
     return (
         <div>
             <For each={objects()}>
-                {(object) => <DocTreeNode object={object} indent={1} />}
+                {(object) => <DocTreeNode object={object} indent={0} />}
             </For>
         </div>
     );
---
+++ bb_web_test/src/features/doc-tree/DocTreeNode.tsx
@@ -0,0 +1,137 @@
+import { DetailedObject } from 'bb_core';
+import { For, Show, createEffect, createMemo, createSignal } from 'solid-js';
+import {
+    TbChevronDown,
+    TbChevronRight,
+    TbEye,
+    TbEyeClosed,
+    TbFocus,
+} from 'solid-icons/tb';
+import { useBobbinBear } from '../../hooks/useBobbinBear';
+import { isDefined } from '~/utils/typeguards';
+
+import { Button } from '../../components/button';
+import { Collapsible, CollapsibleContent } from '~/components/ui/collapsible';
+import { cn } from '~/lib/utils';
+
+type DocTreeNodeProps = {
+    object: DetailedObject;
+    indent: number;
+};
+export function DocTreeNode(props: DocTreeNodeProps) {
+    const { document } = useBobbinBear();
+    const { setVisible, selectSingle } = document;
+    const [expanded, setExpanded] = createSignal(true);
+
+    const childObjects = createMemo(() => {
+        if (!props.object.children) return;
+        const childObjects = props.object.children
+            .map((uid) => document.objects.get(uid))
+            .filter(isDefined);
+        if (childObjects.length === 0) return undefined;
+        else return childObjects;
+    });
+    const showExpandButton = createMemo(() => !!childObjects());
+
+    createEffect(() => {
+        console.log(
+            `object(${props.object.uid}) child objects`,
+            childObjects(),
+            showExpandButton(),
+        );
+    });
+    return (
+        <Collapsible
+            open={expanded()}
+            onOpenChange={(open) => setExpanded(open)}
+        >
+            <div
+                class="outline-yellow-600 hover:outline hover:outline-1"
+                classList={{
+                    'bg-orange-100': props.object.selected,
+                }}
+            >
+                <div
+                    class="flex relative gap-2 items-center py-1 select-none"
+                    style={{ 'margin-left': `${props.indent * 12}px` }}
+                    onClick={async (e) => {
+                        selectSingle(props.object.uid);
+                        e.stopPropagation();
+                    }}
+                >
+                    <DocTreeNodeButtons
+                        showExpandButton={showExpandButton()}
+                        expanded={expanded()}
+                        onExpandedChange={setExpanded}
+                        visible={props.object.visible}
+                        onVisibleChange={(visible) =>
+                            setVisible(props.object.uid, visible)
+                        }
+                    />
+                    {props.object.name ?? 'Unknown'}
+
+                    <Show when={props.object.inspected}>
+                        <div class="absolute right-1 top-1/2 -translate-y-1/2">
+                            <TbFocus />
+                        </div>
+                    </Show>
+                </div>
+            </div>
+            <Show when={childObjects()}>
+                {(children) => (
+                    <CollapsibleContent>
+                        <For each={children()}>
+                            {(object) => (
+                                <DocTreeNode
+                                    object={object}
+                                    indent={props.indent + 1}
+                                />
+                            )}
+                        </For>
+                    </CollapsibleContent>
+                )}
+            </Show>
+        </Collapsible>
+    );
+}
+
+type DocTreeNodeButtonProps = {
+    showExpandButton: boolean;
+    expanded: boolean;
+    onExpandedChange: (expanded: boolean) => void;
+    visible: boolean;
+    onVisibleChange: (visible: boolean) => void;
+};
+function DocTreeNodeButtons(props: DocTreeNodeButtonProps) {
+    return (
+        <div class="flex gap-1 items-center">
+            <Button
+                size="tiny"
+                class={cn(
+                    'bg-transparent bg-opacity-50 hover:bg-orange-50',
+                    !props.showExpandButton && 'opacity-0',
+                )}
+                onClick={(e) => {
+                    e.stopPropagation();
+                    props.onExpandedChange(!props.expanded);
+                }}
+            >
+                <Show when={props.expanded} fallback={<TbChevronRight />}>
+                    <TbChevronDown />
+                </Show>
+            </Button>
+            <Button
+                size="tiny"
+                class="bg-transparent bg-opacity-50 hover:bg-orange-50"
+                onClick={(e) => {
+                    props.onVisibleChange(!props.visible);
+                    e.stopPropagation();
+                }}
+            >
+                <Show when={props.visible} fallback={<TbEyeClosed />}>
+                    <TbEye />
+                </Show>
+            </Button>
+        </div>
+    );
+}
